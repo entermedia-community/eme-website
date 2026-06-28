7b883b751d02ddb2fbb636a7a107654821b14e9f
Shakil Ahmmed <36266736+sXakil@users.noreply.github.com>
2026-06-29 01:03:45 +0600

    removed website plugin

--- plugins/website/html/theme/javascript/website.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-	$("#filterChips").on("click", ".chip", function () {
-		const $chip = $(this);
-		$("#filterChips .chip").removeClass("active");
-		$chip.addClass("active");
-	});
-
-	$("#searchInput").on("input", function () {});
-});
